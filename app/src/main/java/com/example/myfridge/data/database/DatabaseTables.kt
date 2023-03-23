35187a824399816d8fe8876e35fd6261b215c0d1
METrimble <kenzie.trimble@gmail.com>
2023-03-23 09:42:26 -0700

    Finished with add shopping list item

--- app/src/main/java/com/example/myfridge/data/database/DatabaseTables.kt
+++ app/src/main/java/com/example/myfridge/data/database/DatabaseTables.kt
@@ -4,6 +4,7 @@ import android.widget.CalendarView
 import android.widget.DatePicker
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import java.sql.Blob
 
